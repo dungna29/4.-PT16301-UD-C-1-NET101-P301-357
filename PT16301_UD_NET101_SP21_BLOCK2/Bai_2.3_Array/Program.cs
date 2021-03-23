65f19b3a811f88c65fa8627d2f671e21872cfc81
Dung Nguyen Anh (FE FPL HN) <DungNA29@fe.edu.vn>
2021-03-23 07:58:56 +0700

    1

---
+++ PT16301_UD_NET101_SP21_BLOCK2/Bai_2.3_Array/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_2._3_Array
+{
+    class Program
+    {
+        /*
+        * Phần 1: Mảng 1 chiều
+        *
+        *  1. Định nghĩa: Mảng dùng để lưu trữ nhiều phần tử có cùng kiểu dữ liệu (Biến dùng để lưu trữ 1 giá trị có cùng kiểu dữ liệu).
+        *          - Để truy xuất (để lấy giá trị) các phần tử thì cần phải biết chỉ số(index). Index là số nguyên và bắt đầu từ 0 1 2 3....
+        *
+        *  2. Khai báo 1 mảng trong Csharp:
+        *          - Cách 1:
+        *              <Kiểu dữ liệu>[] arr<Tên Mảng>;
+        *              arr<Tên Mảng> = new <Kiểu dữ liệu>[length];//Khởi tạo mảng có kích thước.
+        *          - Cách 2: 
+        *              <Kiểu dữ liệu>[] arr<Tên Mảng> = new <Kiểu dữ liệu>[length];
+        *
+        *          - Cách 3:
+        *     <Kiểu dữ liệu>[] arr<Tên Mảng> = new <Kiểu dữ liệu>[] {value1,value2..,valuen};//Khởi tạo mảng kiểu tường minh.
+        *
+        *     <Kiểu dữ liệu>[] arr<Tên Mảng> = new [] {value1,value2..,valuen};//Khởi tạo kiểu ngầm đinhj
+        *
+        *  3. Truy xuất các phần tử trong mảng
+        *      - Gán giá trị cho mảng:
+        *           <tên mảng>[index] = Giá trị cần gán;
+        *   
+        *     - Lấy giá trị trong mảng:
+        *          <tên mảng>[index] = Giá trị của mảng tại vị trí.
+        *      Lưu ý: Để lấy nhiều giá trị trong mảng xuất ra cần sử dụng vòng lòng.
+        *
+        *  4. Kích thước hoặc độ dài của mảng sử dụng thuộc tính length.
+        *      <tên mảng>.length = kích thước mảng có kiểu số nguyên
+        */
+        static void Main(string[] args)
+        {
+            //Phần 1: Khai báo mảng
+            int[] arrNumber1;//Khai báo 1 mảng
+            arrNumber1 = new int[5];//Khởi tạo mảng có 5 phần tử
+
+            string[] arrNames = new string[5];//Khởi tạo 1 mảng chuỗi có 5 phần tử
+
+            int[] arrYears = new[] {2021, 2022, 2023};
+            double[] arrDiem = { 8.6,7.2,7.3};
+
+            var arrNumber3 = new int[5];//Đây là một mảng int
+
+            //Phần 2: Gán giá trị cho mảng
+            //arrNumber1 = 5; Sai vì không chỉ rõ lưu giá trị vào đâu
+            arrNumber1[0] = 5;//Gán giá trị 5 cho vị trí số 0 trong mảng
+            arrNumber1[1] = 2;
+            arrNumber1[0] = 15;//Ghi đè giá trị
+
+            //Phần 3: Lấy giá trị
+            Console.WriteLine(arrNumber1[0]);//Lấy 1 giá trị đơn
+
+            //Lấy nhiều giá trị trong mảng in ra màn hình
+            for (int i = 0; i < arrNumber1.Length; i++)
+            {
+                Console.Write(arrNumber1[i] +" ");
+            }
+
+            Console.WriteLine();
+            //Vòng lặp foreach fore + tab
+            foreach (int x in arrNumber1)
+            {
+                Console.Write(x +" ");
+            }
+            
+            
+        }
+    }
+}
---
+++ PT16301_UD_NET101_SP21_BLOCK2/Bai_2.5_BaiTongHop/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_2._5_BaiTongHop
+{
+    class Program
+    { /*
+        * Bài tập ôn tập tổng hợp:
+        * Viết 1 chương trình có menu sử dụng switch case và vòng lặp:
+        *         ------Chương trình 1-------
+        *       1. In bảng cửu chương đơn
+        *       2. In bảng cửu theo khoảng
+        *       3. Kiểm tra học lực Sinh Viên Poly
+        *       4. Nhập thông tin sinh viên
+        *       5. Thoát.
+        *
+        * Giải thích:
+        *1. In bảng cửu chương đơn: Mời người dùng nhập vào tên 1 bảng cửu chương mà họ muốn sau đó in ra màn hình.
+        *
+        * 2. In bảng cửu theo khoảng: Mời người dùng nhập vào tên bảng cửu chương theo khoảng. Ví dụ: từ bảng nhân 5 đến nhân 8.
+        *
+        *3. Kiểm tra học lực Sinh Viên Poly: Cho sinh viên POLY nhập vào điểm tổng kết môn C# và thông báo xếp loại học lực:
+        *  diem<=5 --> Yếu
+        *  diem<=7 --> Khá 
+        *  diem<=8 --> Tiên Tiến
+        *  diem<=9 --> Giỏi
+        *  diem<=10 --> Xuất sắc
+        *
+          4. Nhập thông tin nhiều sinh viên gồm các thông tin sau: Tên, Mã Sinh Viên, Năm Sinh. In thông tin sinh viên ra 
+            Tên + Mã Sinh Viên + Năm Sinh + Tuổi.
+        */
+        static void Main(string[] args)
+        {
+            //Nhập vào 1 dãy số nguyên sau đó in ra màn hình
+            int[] arrNumber;
+            int sizeArr;
+            Console.Write("Bạn muộn nhập bao nhiêu số: ");
+            sizeArr = Convert.ToInt32(Console.ReadLine());//Lấy được kích thước mảng do người dùng nhập vào
+            arrNumber = new int[sizeArr];//Khởi tạo kích thước mảng sau khi người dùng nhập vào
+            for (int i = 0; i < sizeArr; i++)
+            {
+                Console.WriteLine("Mời bạn nhập số thứ " + i);
+                arrNumber[i] = Convert.ToInt32(Console.ReadLine());//Gán giá trị cho mảng
+            }
+
+            Console.WriteLine("Dãy số bạn vừa nhập vào là: ");
+            foreach (var x in arrNumber)
+            {
+                Console.Write(x + " ");
+            }
+        }
+    }
+}
